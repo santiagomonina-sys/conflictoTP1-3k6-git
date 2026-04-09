b669b7a01d431da209c899d591bfd5307b81d442
Santiago Monina <404697@sistemas.frc.utn.edu.ar>
2026-04-09 17:49:27 -0300

    conflicto resuelto

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
 function saludar() {
-<<<<<<< HEAD
-<<<<<<< HEAD
-    alert("Hola ");
-=======
+
     alert("Hola bienvenido");
->>>>>>> rama1
 }
-=======
-    alert("Hola, bienvenido");
-}
->>>>>>> rama2
